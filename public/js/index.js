81f1433836f11adb61fd36f295553a497ec56285
PurplePachyderm <Vous@exemple.com>
2018-12-30 00:22:56 +0100

    Update interface for upcoming features

--- public/js/index.js
+++ public/js/index.js
@@ -8,8 +8,7 @@ syncBtn  = document.querySelector('#syncBtn'),
 asyncBtn = document.querySelector('#asyncBtn');
 
 var data = {
-    peers: [],
-    selectedPeer:'',
+    peerId: '',
     conn: '',
     connected: false,
     peerConnected: false,
@@ -33,55 +32,53 @@ var app = new Vue({
     mounted: () => {
         console.log("Welcome to the Ultimate P2P testing interface");
 
-            // Set P2P & socket.io connection
+            // Set socket.io connection
         socket = io.connect('http://127.0.0.1:8080');
         socket.on('connect', () => {
             console.log("Session socket ID is: "+socket.io.engine.id);
         });
 
+
+            //Connect to Peer.js
         peer = new Peer({key: 'lwjd5qra8257b9'});   //TODO Get a real key somehow :p
         peer.on('open', function(id) {
             console.log('Session peer ID is: ' + id);
-
-            //Send peer ID to server
-            socket.emit("newPeerId", {peerId: id, socketId: socket.io.engine.id});
+            data.peerId = id;
+            console.log(data.peerId);
         });
 
 
             //User logged in
         socket.on('signInSuccess', (user) => {
+                //Set Vue data
             data.connected = true;
             data.user.fiName = user.fiName;
             data.user.faName = user.faName;
             data.user.pseudo = user.pseudo;
             data.user.password = '';
 
+                //Send peer ID to server
+            socket.emit("newPeerId", {peerId: data.peerId, socketId: socket.io.engine.id});
+
+                //Request files
             ipc.send('signInSuccess', {email: data.user.email});
             console.log("Sign in successfull!")
         });
+
+
 
         socket.on('signUpFailure', (user) => {
             console.log("Fuck, something went wrong!");
         });
 
+
+
             // Receive file list from main
-        ipc.on('filesList', (event, data) => {
-            data.files = data.list;
-            console.log("Received files:");
-            console.log(data.files);
-            console.log("We have "+data.files.length+" fucking files");
+        ipc.on('filesList', (event, newData) => {
+            data.files = newData.list;
+            console.log("Received files!");
         });
 
-
-
-
-
-            //New user notice
-        socket.on("newPeerId", (newData) => {
-            console.log("New connected: "+newData.peerId);
-
-            data.peers.push(newData);
-        });
 
 
             //Accept incoming connections
@@ -97,12 +94,19 @@ var app = new Vue({
             shaObj.update(data.user.password);
             var hash= shaObj.getHash("HEX");
 
-            socket.emit('signIn', {password: hash, email: data.user.email});
+            socket.emit('signIn', {password: hash, email: data.user.email, socketId: socket.io.engine.id});
         },
 
-        connectPeer: () => {
-            console.log(data.selectedPeer);
-            data.conn = peer.connect(data.selectedPeer);
+        logout: () => {
+            data.connected = false;
+            data.files = [];
+
+            socket.emit('signOut', {socketId: socket.io.engine.id});
+        },
+
+        connectPeer: (peerId) => {
+            console.log(peerId);
+            data.conn = peer.connect(peerId);
             data.peerConnected = true;
         }
 
